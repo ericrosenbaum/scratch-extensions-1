59be837de4b0a46e157df18dad97bdb61da21180
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-01-22 14:22:34 -0500

    add ‘recognized numbers’ with parsing and conversion

--- speech_io_extension.js
+++ speech_io_extension.js
@@ -26,7 +26,87 @@ new (function() {
         recognition.start();
     };
 
-    ext.recognized_speech = function () {return recognized_speech;};
+    ext.get_recognized_speech = function () {
+        return recognized_speech;
+    };
+
+    ext.get_recognized_number = function () {
+        // convert a number name anywhere in the string to a number
+        var converted = wordsToNumbers(recognized_speech); 
+        if (typeof converted == 'number') {
+            // console.log('converted to: ' + converted);
+            return converted;
+        }
+
+        // extract numbers from the string
+        var extracted = extractNumber(recognized_speech);
+        if (typeof extracted == 'number') {
+            // console.log('extracted: ' + extracted);
+            return extracted;
+        }
+
+        return '';
+    }
+
+    // convert the string names of numbers to numbers (up to twenty)
+    // if there are multiple names in the string, return the last one
+    function wordsToNumbers (string) {
+        var numberNames = [
+            'zero',
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven',
+            'eight',
+            'nine',
+            'ten',
+            'eleven',
+            'twelve',
+            'thirteen',
+            'fourteen',
+            'fifteen',
+            'sixteen',
+            'seventeen',
+            'eighteen',
+            'nineteen',
+            'twenty'
+        ];
+
+        var result = 'none';
+        for (var i=0; i<numberNames.length; i++) {
+            if (string.includes(numberNames[i])) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    // extract the first contiguous set of numbers in the string
+    // this could be done more compactly with a regex match, but there is some problem
+    // with using the excape chars in the extension
+    function extractNumber (string) {
+        var numString = '';
+        var numStartedFlag = false;
+        for (var i=0; i < string.length; i++) {
+            var maybeNum = parseInt(string[i]);
+            if (!isNaN(maybeNum)) {
+                numString += maybeNum;
+                numStartedFlag = true;
+            } else {
+                if (numStartedFlag) {
+                    break;
+                }
+            }
+        }
+        if (numString.length > 0) {
+            return Number(numString);
+        } else {
+            return 'none';
+        }
+    }
 
     ext._shutdown = function() {};
 
@@ -40,8 +120,9 @@ new (function() {
     var descriptor = {
         blocks: [
             ['w', 'speak %s', 'speak_text', 'Hello!'],
-            ['w', 'wait and recognize speech', 'recognize_speech'],
-            ['r', 'recognized speech', 'recognized_speech']
+            ['w', 'listen for speech and wait', 'recognize_speech'],
+            ['r', 'recognized speech', 'get_recognized_speech'],
+            ['r', 'recognized number', 'get_recognized_number']
         ],
     };
 
